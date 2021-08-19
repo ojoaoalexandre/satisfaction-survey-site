d026b0b60a49c595edb1e88371da656bcb014a58
Alexandre Bekor <contato@alexandrebekor.com>
2021-08-19 20:11:00 -0300

    Start Pages: Sobre, Contato and Pesquisa

---
+++ pages/contato.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Contato = () => {
+    return (
+        <div>
+            <h1>Contato</h1>
+            <div>
+                <Link href="/">
+                    <a>Início</a>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Contato
---
+++ pages/pesquisa.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Pesquisa = () => {
+    return (
+        <div>
+            <h1>Pesquisa</h1>
+            <div>
+                <Link href="/">
+                    <a>Início</a>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Pesquisa
