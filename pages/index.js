67ae71eeb563d39076352d20706408b51a8f5679
Alexandre Bekor <contato@alexandrebekor.com>
2021-08-19 20:05:17 -0300

    Menu Index

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Index = () => {
+    return (
+        <div>
+            <h1>Início</h1>
+            <div>
+                <Link href="/sobre">
+                    <a>Sobre</a>
+                </Link>
+            </div>
+            <div>
+                <Link href="/contato">
+                    <a>Contato</a>
+                </Link>
+            </div>
+            <div>
+                <Link href="/pesquisa">
+                    <a>Pesquisa</a>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Index
