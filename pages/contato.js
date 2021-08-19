894aab1479a2580923269aabe575a37b604d7ff2
Alexandre Bekor <jvalexandre.sd@gmail.com>
2021-08-19 13:55:02 +0000

    PagPageContato Page

--- pages/contato.js
+++ pages/contato.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import Link from 'next/link'
 
-const Index = () => {
+const Contato = () => {
     return (
         <div>
-            <h1>Hello World!</h1>
+            <h1>Contato</h1>
             <div>
-                <Link href='/contato'>
-                    <a>Contato</a>
+                <Link href='/'>
+                    <a>Início</a>
                 </Link>
             </div>
         </div>
     )
 }
 
-export default Index
+export default Contato
