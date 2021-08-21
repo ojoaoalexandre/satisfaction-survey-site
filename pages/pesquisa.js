e6f7639d84c69fc687d3b74a382b2936223ffe26
Alexandre Bekor <contato@alexandrebekor.com>
2021-08-21 12:16:06 -0300

    API save

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -2,14 +2,30 @@ import React from 'react'
 import Link from 'next/link'
 
 const Pesquisa = () => {
+    const save = async () => {
+        const form = {
+            Nome: 'Alexandre Bekor',
+            Email: 'staff@agenciabekor.com',
+            Whatsapp: '11000000000'
+        }
+        try {
+            const response = await fetch('/api/save', {
+                method: 'POST',
+                body: JSON.stringify(form)
+            })
+            const data = await response.json()
+            console.log(data)
+        } catch (err) {
+            console.log(data)
+        }
+    }
+
     return (
         <div>
             <h1>Pesquisa</h1>
-            <div>
-                <Link href="/">
-                    <a>Início</a>
-                </Link>
-            </div>
+            <label>Nome:</label>
+            <input type="text" />
+            <button onClick={ save }>Enviar</button>
         </div>
     )
 }
