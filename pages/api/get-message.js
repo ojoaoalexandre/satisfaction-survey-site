f15e1f478cd3931a0d6929eae286296fb0e833cb
Alexandre Bekor <contato@alexandrebekor.com>
2021-08-22 00:10:22 -0300

    API get-message connect Google Spreadsheet

--- pages/api/get-message.js
+++ pages/api/get-message.js
@@ -1,6 +1,29 @@
+import { GoogleSpreadsheet } from "google-spreadsheet"
+import credentials from '../../credentials.json'
+
+const doc = new GoogleSpreadsheet('1VBSCBiaryRYSIFHBrPOKoCHQyvtOU2V0IMedVjgl9ow')
+
 export default async(req, res) => {
-    res.end(JSON.stringify({
-        showMessage: true,
-        message: 'Essa é a mensagem'
-    }))
+
+    try{
+        await doc.useServiceAccountAuth(credentials)
+        await doc.loadInfo()
+
+        const sheet = doc.sheetsByIndex[0]
+        await sheet.loadCells('A2:B2')
+
+        const showMessageSheet = sheet.getCell(1, 0)
+        const messageSheet = sheet.getCell(1, 1)
+        
+        res.end(JSON.stringify({
+            showMessage: showMessageSheet.value === 'VERDADEIRO',
+            message: messageSheet.value
+        }))
+    } catch (err) {
+        res.end(JSON.stringify({
+            showMessage: false,
+            message: ''
+        }))
+    }
+
 }
